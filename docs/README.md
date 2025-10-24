f3e11749ffc35226959759815aadb26b2ebc41a9
Yuki20-0202 <131835968+Yuki20-0202@users.noreply.github.com>
2025-10-24 22:39:50 +0900

    Add files via upload

---
+++ docs/README.md
@@ -0,0 +1,2 @@
+# PBL1_2
+https://yuki20-0202.github.io/
---
+++ docs/top1/top1_cript.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+  const menu = document.getElementById("side-menu");
+  if (menu.style.left === "0px") {
+    menu.style.left = "-250px";
+  } else {
+    menu.style.left = "0px";
+  }
+}
+function goToPage(pageName) {
+  window.location.href = pageName;
+}
